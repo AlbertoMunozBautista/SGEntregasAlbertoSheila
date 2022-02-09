d685762cc03a3ef17cb49f3d3985b5d5e57f587a
Sheila <sheilanievalo.2000@gmail.com>
2022-02-09 18:52:05 +0100

    Btn Eliminar Diseño modificar cliente

--- SGEntregasAlbertoSheila/GestionClientesVentana.xaml.cs
+++ SGEntregasAlbertoSheila/GestionClientesVentana.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -55,14 +56,57 @@ namespace SGEntregasAlbertoSheila
 
         private void ejecutaGuardarBD(object sender, ExecutedRoutedEventArgs e)
         {
-
             cvm.guardarDatos();
-            //cvm.objBD.SaveChanges();
+            System.Windows.MessageBox.Show("Se ha guardado en la BBDD", "Éxito");
         }
 
         private void compruebaGuardarBD(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
+        }
+
+        private void ejecutaModificar(object sender, ExecutedRoutedEventArgs e)
+        {
+            ModificarCliente modificarCliente = new ModificarCliente(cvm);
+            modificarCliente.Show();
+           
+        }
+
+        private void compruebaModificar(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (lstClientes.SelectedIndex != -1)
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void compruebaEliminar(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (lstClientes.SelectedIndex != -1)
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void ejecutaEliminar(object sender, ExecutedRoutedEventArgs e)
+        {
+            DialogResult dr = (DialogResult)System.Windows.MessageBox.Show("¿Estás seguro?", "ÉXITO", MessageBoxButton.YesNo);
+
+            String idCliente = cvm.ListaClientes[lstClientes.SelectedIndex].dni;
+
+            clientes objCliente = cvm.objBD.clientes.Find(idCliente);
+
+            if (dr == System.Windows.Forms.DialogResult.Yes)
+            {
+                while (objCliente.pedidos.Count > 0)
+                {
+                    var pedido = (pedidos)objCliente.pedidos.First();
+                    cvm.objBD.pedidos.Remove(pedido);
+                }
+
+                cvm.ListaClientes.RemoveAt(lstClientes.SelectedIndex);
+                cvm.objBD.clientes.Remove(objCliente);
+            }
         }
 
     }
---
+++ SGEntregasAlbertoSheila/ModificarCliente.xaml.cs
@@ -0,0 +1,89 @@
+﻿using SGEntregasAlbertoSheila.ViewModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGEntregasAlbertoSheila
+{
+    /// <summary>
+    /// Lógica de interacción para ModificarCliente.xaml
+    /// </summary>
+    public partial class ModificarCliente : Window
+    {
+
+        CollectionViewModel cvm;
+        ArrayList listaProvincias = new ArrayList();
+        //private clientes cliente;
+        //private clientes copCliente;
+        public ModificarCliente(CollectionViewModel cvm)
+        {
+            InitializeComponent();
+            this.cvm = cvm;
+            //ocultar botones
+            this.WindowStyle = WindowStyle.None;
+
+            //no redimensionable
+            this.ResizeMode = ResizeMode.NoResize;
+
+            //centrar pantalla
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            cargarComboProvincias();
+        }
+
+        private void cargarComboProvincias()
+        {
+            using (entregasEntities objBD = new entregasEntities())
+            {
+                this.cmbProvincia.Items.Clear();
+                var qProvincias = from pro in objBD.provincias select pro;
+
+                foreach (var pro in qProvincias.ToList())
+                {
+                    cmbProvincia.Items.Add(pro.nombre_provincia);
+                    listaProvincias.Add(pro.id_provincia);
+                }
+            }
+        }
+
+        private void ejecutaAceptar(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            
+
+        }
+
+        private void compruebaAceptar(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+            if (txtNombre.Text.Trim() != "" && txtApellidos.Text.Trim() != "" && txtEmail.Text.Trim() != "" && txtDni.Text.Trim() != "" && txtLocalidad.Text.Trim() != "" && txtDomicilio.Text.Trim() != "")
+            {
+                e.CanExecute = true;
+            }
+
+
+        }
+
+        private void ejecutaCancelar(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void compruebaCancelar(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
+
