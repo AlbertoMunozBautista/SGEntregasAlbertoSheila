52741a9116db9ef246a4c3a7cd7a38058688f29b
Alberto Muñoz Bautista <amuozb08@educastillalamancha.es>
2022-02-10 15:53:39 +0100

    pedidos + principio de ventana de pedidos

--- SGEntregasAlbertoSheila/ViewModel/CollectionViewModel.cs
+++ SGEntregasAlbertoSheila/ViewModel/CollectionViewModel.cs
@@ -12,7 +12,11 @@ namespace SGEntregasAlbertoSheila.ViewModel
     public class CollectionViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
         private ClientesCollection _listaClientes = new ClientesCollection();
+        private PedidosCollection _listaPedidos = new PedidosCollection();
+
+
         private entregasEntities _objBD = new entregasEntities();
 
         public CollectionViewModel()
@@ -23,10 +27,19 @@ namespace SGEntregasAlbertoSheila.ViewModel
         private void cargarDatos()
         {
             ListaClientes.Clear();
+            ListaPedidos.Clear();
+
             var qClientes = from cli in objBD.clientes select cli;
             foreach (var clien in qClientes.ToList())
             {
                 ListaClientes.Add(clien);
+            }
+
+            var qPedidos = from ped in objBD.pedidos select ped;
+
+            foreach (var ped in qPedidos.ToList())
+            {
+                ListaPedidos.Add(ped);
             }
         }
 
@@ -36,6 +49,16 @@ namespace SGEntregasAlbertoSheila.ViewModel
             set
             {
                 _listaClientes = value;
+                raisePropertyChanged();
+            }
+        }
+
+        public PedidosCollection ListaPedidos
+        {
+            get { return _listaPedidos; }
+            set
+            {
+                _listaPedidos = value;
                 raisePropertyChanged();
             }
         }
