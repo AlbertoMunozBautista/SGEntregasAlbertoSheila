c2743761d29f72858a847f27e8ea5c289ffbdbdd
alberto <albertomunozbautista@gmail.com>
2022-02-12 01:25:04 +0100

    retoques en los layouts + otro botón atrás

--- SGEntregasAlbertoSheila/GestionPedidosVentana2.xaml.cs
+++ SGEntregasAlbertoSheila/GestionPedidosVentana2.xaml.cs
@@ -115,5 +115,19 @@ namespace SGEntregasAlbertoSheila
                 e.CanExecute = true;
             }
         }
+
+
+        private void ejecutaAtras(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+            MenuOrdenadorVentana menuOrdenadorVentana = new MenuOrdenadorVentana();
+            menuOrdenadorVentana.Show();
+
+        }
+
+        private void compruebaAtras(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
     }
 }
