7e92dbe98958c2d480b972931bd9f1a6de1e52ae
Sheila <sheilanievalo.2000@gmail.com>
2022-02-21 21:28:55 +0100

    Comentarios en primeraTabletVentana

--- SGEntregasAlbertoSheila/PrimeraTabletVentana.xaml.cs
+++ SGEntregasAlbertoSheila/PrimeraTabletVentana.xaml.cs
@@ -40,29 +40,36 @@ namespace SGEntregasAlbertoSheila
             cvm = (CollectionViewModel)this.Resources["ColeccionVM"];
         }
 
+        //Se ejecuta al pulsar el boton 'atras'
         private void ejecutaAtras(object sender, ExecutedRoutedEventArgs e)
         {
+            //Cierra esta ventana y se abre la principal (la anterior)
             this.Close();
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
 
         }
 
+        //Llama a ejecutaAtras()
         private void compruebaAtras(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
         }
 
+        //Metodo que se ejecuta al cambiar la seleccion de la lista de clientes
         private void lstClientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //Rescatamos el dni de cliente segun el item seleccionado
             String dniCliente = cvm.ListaClientes[lstClientes.SelectedIndex].dni;
 
+            //Hacemos una consulta para obtener los pedidos de un cliente en concreto y que no se haya entregado
             var pedidosVacios = from pe in cvm.objBD.pedidos
                                 where pe.cliente.Equals(dniCliente) && pe.fecha_entrega == null
                                 select pe;
-
+            //Si esta consulta nos da registros
             if (pedidosVacios.Count() > 0)
             {
+                //Cerramos esta ventana y abrimos segundaTabletVentana (pasandole el dni del cliente)
                 this.Close();
                 SegundaTabletVentana segundaTabletVentana = new SegundaTabletVentana(dniCliente);
                 segundaTabletVentana.ShowDialog();
@@ -71,11 +78,6 @@ namespace SGEntregasAlbertoSheila
             {
                 MessageBox.Show("Este cliente no tiene ningun pedido para firmar");
             }
-
         }
-
-
     }
-
-
 }
