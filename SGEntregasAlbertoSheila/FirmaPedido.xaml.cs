fa8d335f1f99e046815e1731764f1b8be443ae63
Sheila <sheilanievalo.2000@gmail.com>
2022-02-21 20:47:32 +0100

    Comentarios firma pedido

--- SGEntregasAlbertoSheila/FirmaPedido.xaml.cs
+++ SGEntregasAlbertoSheila/FirmaPedido.xaml.cs
@@ -23,6 +23,7 @@ namespace SGEntregasAlbertoSheila
     /// </summary>
     public partial class FirmaPedido : Window
     {
+        //Declaracion de variables
         DateTime fechaHoy = DateTime.Now;
         pedidos pedido;
         pedidos copiaPedido;
@@ -33,14 +34,15 @@ namespace SGEntregasAlbertoSheila
         clientes cli;
 
         byte[] firmaByte;
+
+        //Constructor. Recibimos un pedido, el collectionviewmodel y la ventana de pedidos
         public FirmaPedido(pedidos pedido, CollectionViewModel cvm, SegundaTabletVentana ventanaPedidos)
         {
             InitializeComponent();
 
+            //Debemos tener otra variable con un pedido clonado y asignarlo al DataContext para hacer modificaciones
             this.pedido = pedido;
-
             copiaPedido = (pedidos)pedido.Clone();
-
             this.DataContext = copiaPedido;
 
             this.WindowStyle = WindowStyle.None;
@@ -50,78 +52,72 @@ namespace SGEntregasAlbertoSheila
 
             //centrar pantalla
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //Metemos la fecha de hoy automaticamente en el campo fecha de entrega de la vetana
             txtFechaEntrega.Text = fechaHoy.ToString("dd/MM/yyyy");
 
             this.ventanaPedidos = ventanaPedidos;
-
             this.cvm = cvm;
-
+            //Usamos la clase 'clientes' para usar el metodo de cargarCliente()
             this.cli = cargarCliente();
         }
 
         private clientes cargarCliente()
         {
+            //Hacemos una consulta en los clientes donde buscamos el cliente que este enlazado con el pedido que hemos seleccionado
             clientes objCliente = cvm.objBD.clientes.Find(this.pedido.cliente);
+            //De este consulta obtenemos el apellido y el nombre, que es lo que vamos a mostrar en esta ventana como información
             txtCliente.Text = objCliente.apellidos + ", " + objCliente.nombre;
 
             return objCliente;
-
         }
 
+        //Se cierra la ventana si pulsamos en el boton cancelar
         private void btnCancelar_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        //Metodo que limpia el cuadro de la firma si pulsamos el boton 'Limpiar'
         private void btnLimpiar_Click(object sender, RoutedEventArgs e)
         {
             firmaCanvas.Strokes.Clear();
         }
 
+        //Metodo que cuando pulsamos el boton aceptar
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
         {
-            //TODO COMPROBAR FIRMA
+            //Comprueba que el cuadro de firma no este vacio
             if (firmaCanvas.Strokes.Count > 0) {
 
+                //Llamamos al metodo InkCanvasToByte y lo recogemos en un array de bytes (firmaByte)
                 firmaByte = InkCanvasToByte();
 
+                //Metemos la fecha de hoy y la firma en el objeto clonado del pedido (copiaPedido)
                 copiaPedido.fecha_entrega = fechaHoy;
                 copiaPedido.firma = firmaByte;
 
+                //Metodo dnd le pasamos la copia(lo que hemos modificado) y el pedido(que habiamos recibido en principio)
                 actualizarProperties(copiaPedido, pedido);
 
+                //Llamamos al metodo guardarDatos() del CollectionViewModel para guardar en la bbdd los cambios
                 cvm.guardarDatos();
 
                 MessageBox.Show("Guardado en la bbdd correctamente la entrega del pedido");
 
+                //Volvemos a llamar a cargarTarjeta() de la ventana Pedidos. Ahi es vuelven a cargar los pedidos
+                //y se muestran solo los pedidos que no estén firmados.
                 ventanaPedidos.cargarTarjeta();
 
-                this.Close();
-
-               
-
-                //PrimeraTabletVentana primeraTabletVentana = new PrimeraTabletVentana();
-                //primeraTabletVentana.Show();
-            } else
+                this.Close();//Cerramos la ventana
+            } 
+            //Si la firma no está rellena mostramos un mensaje de informacion
+            else
             {
                 MessageBox.Show("La firma es obligatoria");
             }
-
-            
-            /*
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "isf files (*.isf)| *.isf";
-            if (saveFileDialog1.ShowDialog() == true)
-            {
-                FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create);
-                firmaCanvas.Strokes.Save(fs);
-                fs.Close();
-            }*/
-
-          
-
         }
 
+        //Metodo que pasa la firma del InkCanvas a un array de byres
         private byte[] InkCanvasToByte()
         {
             using (MemoryStream ms = new MemoryStream())
@@ -139,7 +135,8 @@ namespace SGEntregasAlbertoSheila
             }
         }
 
-
+        //Metodo que actualiza el pedido con los cambios realizados
+        //Pasamos los datos de pedidoOrigen(copiaPedido, lo que hemos modificado) a pedidoDestino(pedido, que vamos a guardar en la bbbdd modificado)
         private void actualizarProperties(pedidos pedidoOrigen, pedidos pedidoDestino)
         {
             pedidoDestino.cliente = pedidoOrigen.cliente;
