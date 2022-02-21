6a457904c72277624f88312de1a3afc3d5b3d6a9
Sheila <sheilanievalo.2000@gmail.com>
2022-02-21 21:09:51 +0100

    Comentarios de GestionPedidosVentana

--- SGEntregasAlbertoSheila/GestionPedidosVentana.xaml.cs
+++ SGEntregasAlbertoSheila/GestionPedidosVentana.xaml.cs
@@ -21,14 +21,17 @@ namespace SGEntregasAlbertoSheila
     /// </summary>
     public partial class GestionPedidosVentana : Window
     {
+        //Declaracion de variables
         CollectionViewModel cvm;
         ArrayList listaClientes = new ArrayList();
 
         MenuOrdenadorVentana menuOrdenador;
         public GestionPedidosVentana(MenuOrdenadorVentana menuOrdenador)
         {
-            InitializeComponent();
+         InitializeComponent();
+            //Llamaos de nuevo al ColeectionViewModel, declarado en el codigo de xaml.
             cvm = (CollectionViewModel)this.Resources["ColeccionVM"];
+
             this.WindowStyle = WindowStyle.None;
 
             //no redimensionable
@@ -38,41 +41,52 @@ namespace SGEntregasAlbertoSheila
 
             //centrar pantalla
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
             cargarComboClientes();
         }
 
+        //Carga el combo con los clientes que haya en la bbdd
         private void cargarComboClientes()
         {
             using (entregasEntities objBD = new entregasEntities())
             {
                 this.cmbClientes.Items.Clear();
+                //Consulta de los todos los clientes en la bbdd y sacamos el apellido y el nombre
                 var qClientes = from cli in objBD.clientes orderby cli.apellidos, cli.nombre select cli;
 
                 foreach (var cli in qClientes.ToList())
                 {
                     cmbClientes.Items.Add(cli.apellidos + ", " + cli.nombre);
-                    listaClientes.Add(cli.dni);
+                    listaClientes.Add(cli.dni);//Lista que recoge todos los dni del cliente en el mismo orden que estan en el combobox
                 }
-                cmbClientes.SelectedIndex = 0;
+                cmbClientes.SelectedIndex = 0;//por defecto mostramos el primer cliente en el combobox
             }
         }
 
+        //Boton confirmar
         private void btnConfirmar_Click(object sender, RoutedEventArgs e)
         {
+            //Cogemos la posicion del combobox seleccionado
             int pos = cmbClientes.SelectedIndex;
+            //Recuperamos el dni de ese cliente por la lista que obtiene los dni de los clientes
             string dniClienteSeleccionado = listaClientes[pos].ToString();
+            //Busvamos el cliente en la bbdd por su dni
             clientes objCliente = cvm.objBD.clientes.Find(dniClienteSeleccionado);
+            //Cerramos vetanas
             menuOrdenador.Close();
             this.Close();
+            //Abrimos la ventana gestionPedidosVentan2 pasandole el cliente seleccionado
             GestionPedidosVentana2 gestionPedidos2 = new GestionPedidosVentana2(objCliente);
             gestionPedidos2.ShowDialog();
         }
 
+        //Cierra esta ventana
         private void ejecutaCancelar(object sender, ExecutedRoutedEventArgs e)
         {
             this.Close();
         }
 
+        //Llama al metodo ejecutaCancelar()
         private void compruebaCancelar(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
